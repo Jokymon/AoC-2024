3efe4e0385d3795126048885df568de55cdb8985
Silvan Wegmann <silvan.wegmann@hslu.ch>
2024-12-06 17:56:19 +0100

    Add base for day7

---
+++ src/day7/day7.rs
@@ -0,0 +1,41 @@
+
+use std::error::Error;
+use std::fs::read_to_string;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SIMPLE_INPUT : &str = r#"
+    "#;
+
+    #[test]
+    fn test_simple_input_part1() {
+        assert_eq!(challenge1(SIMPLE_INPUT), 0);
+    }
+
+    #[test]
+    fn test_simple_input_part2() {
+        assert_eq!(challenge2(SIMPLE_INPUT), 0);
+    }
+}
+
+fn challenge1(challenge_input: &str) -> i32 {
+    42
+}
+
+fn challenge2(challenge_input: &str) -> i32 {
+    42
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let test_input = read_to_string("input_data/day7/input.txt")?;
+
+    let result1 = challenge1(&test_input);
+    let result2 = challenge2(&test_input);
+
+    println!("Answer part 1: {}", result1);
+    println!("Answer part 2: {}", result2);
+
+    Ok(())
+}
