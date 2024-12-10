2f4e158ee70ef5210e2a127c66de3e87d14c6980
Silvan Wegmann <silvan.wegmann@hslu.ch>
2024-12-10 15:26:08 +0100

    Implement part 2 for day 7

--- src/day7/day7.rs
+++ src/day7/day7.rs
@@ -23,7 +23,24 @@ mod tests {
 
     #[test]
     fn test_simple_input_part2() {
-        assert_eq!(challenge2(SIMPLE_INPUT), 0);
+        assert_eq!(challenge2(SIMPLE_INPUT), 11387);
+    }
+
+    #[test]
+    fn test_concatenation() {
+        assert_eq!(concat_i64(34, 234), 34234);
+        assert_eq!(concat_i64(17, 100), 17100);
+    }
+
+    #[test]
+    fn test_select_digit() {
+        assert_eq!(select_digit(0b0100101, 2, 2), 1);
+        assert_eq!(select_digit(0b0100101, 0, 2), 1);
+
+        assert_eq!(select_digit(0 + 3*2 + 9*1 + 27*2, 0, 3), 0);
+        assert_eq!(select_digit(0 + 3*2 + 9*1 + 27*2, 1, 3), 2);
+        assert_eq!(select_digit(0 + 3*2 + 9*1 + 27*2, 2, 3), 1);
+        assert_eq!(select_digit(0 + 3*2 + 9*1 + 27*2, 3, 3), 2);
     }
 }
 
@@ -74,8 +91,38 @@ fn challenge1(challenge_input: &str) -> i64 {
     sum
 }
 
-fn challenge2(_challenge_input: &str) -> i32 {
-    42
+fn concat_i64(a: i64, b: i64) -> i64 {
+    // Code "borrowed" from https://stackoverflow.com/questions/69297477/getting-the-length-of-an-int
+    let digits_b = b.checked_ilog10().unwrap_or(0) + 1;
+    i64::pow(10, digits_b)*a + b
+}
+
+fn select_digit(number: i64, index: usize, base: i64) -> i64 {
+    (number / i64::pow(base, index as u32)) % base
+}
+
+fn challenge2(challenge_input: &str) -> i64 {
+    let input = parse_input(challenge_input);
+    let mut sum = 0;
+    const OPERATIONS: [fn(i64, i64) -> i64; 3] = [i64::wrapping_add, i64::wrapping_mul, concat_i64];
+    input.iter().for_each(|eq| {
+        let possiblities = i64::pow(3, (eq.numbers.len() - 1) as u32);
+        for combination in 0..possiblities {
+            let mut possibility_sum = eq.numbers[0];
+
+            for op_position in 0..eq.numbers.len() - 1 {
+                let op_index = select_digit(combination, op_position, 3);
+                possibility_sum =
+                    OPERATIONS[op_index as usize](possibility_sum, eq.numbers[op_position + 1]);
+            }
+
+            if possibility_sum == eq.test {
+                sum += eq.test;
+                break
+            }
+        }
+    });
+    sum
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
