6cc9057f73fcb6cb98ca2c13abc1d31ee42e6fba
Silvan Wegmann <silvan.wegmann@hslu.ch>
2024-12-11 13:19:37 +0100

    Solve part 1 of day 11

--- src/day11/day11.rs
+++ src/day11/day11.rs
@@ -1,16 +1,18 @@
+use aoc2024::SimpleParse;
 use std::error::Error;
 use std::fs::read_to_string;
+use count_digits::CountDigits;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const SIMPLE_INPUT: &str = r#"
+    const SIMPLE_INPUT: &str = r#"125 17
     "#;
 
     #[test]
     fn test_simple_input_part1() {
-        assert_eq!(challenge1(SIMPLE_INPUT), 0);
+        assert_eq!(challenge1(SIMPLE_INPUT), 55312);
     }
 
     #[test]
@@ -19,8 +21,27 @@ mod tests {
     }
 }
 
-fn challenge1(_challenge_input: &str) -> i64 {
-    42
+fn challenge1(challenge_input: &str) -> i64 {
+    let mut stones: Vec<_> = challenge_input
+        .trim()
+        .split_whitespace()
+        .map(str::get_i64)
+        .collect();
+    for _ in 0..25 {
+        stones = stones.iter().flat_map(|stone| {
+            match *stone {
+                0 => vec![1],
+                stone if stone.count_digits()%2==0 => {
+                    let total_digits = stone.count_digits();
+                    let part1 = stone / i64::pow(10, (total_digits/2) as u32);
+                    let part2 = stone % i64::pow(10, (total_digits/2) as u32);
+                    vec![part1, part2]
+                },
+                _ => vec![*stone * 2024],
+            }
+        }).collect();
+    }
+    stones.iter().count() as i64
 }
 
 fn challenge2(_challenge_input: &str) -> i64 {
