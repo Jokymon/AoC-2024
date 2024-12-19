b3542e236e82ff78cb4379dc18194861e6f9f84e
Silvan Wegmann <silvan.wegmann@hslu.ch>
2024-12-19 09:40:59 +0100

    Implement day 19 for part 1

--- src/day19/day19.rs
+++ src/day19/day19.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use regex::Regex;
 use std::error::Error;
 use std::fs::read_to_string;
 
@@ -5,12 +7,20 @@ use std::fs::read_to_string;
 mod tests {
     use super::*;
 
-    const SIMPLE_INPUT: &str = r#"
-    "#;
+    const SIMPLE_INPUT: &str = r#"r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"#;
 
     #[test]
     fn test_simple_input_part1() {
-        assert_eq!(challenge1(SIMPLE_INPUT), 0);
+        assert_eq!(challenge1(SIMPLE_INPUT), 6);
     }
 
     #[test]
@@ -19,8 +29,23 @@ mod tests {
     }
 }
 
-fn challenge1(_challenge_input: &str) -> i64 {
-    42
+fn parse_input(input: &str) -> (regex::Regex, Vec<&str>) {
+    let (available_patterns, desired_designs) = input.splitn(2, "\n\n").collect_tuple().unwrap();
+
+    let regex_string = format!("^({})+$", available_patterns.split(",").map(str::trim).join("|"));
+
+    (
+        Regex::new(&regex_string).unwrap(),
+        desired_designs.split_whitespace().collect(),
+    )
+}
+
+fn challenge1(challenge_input: &str) -> i64 {
+    let (towel_rules, designs) = parse_input(challenge_input);
+
+    designs.iter().filter(|design| {
+        towel_rules.is_match(design)
+    }).count() as i64
 }
 
 fn challenge2(_challenge_input: &str) -> i64 {
